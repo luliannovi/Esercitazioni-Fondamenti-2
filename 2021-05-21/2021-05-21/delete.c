edd0c5f26ab0119ec19800b51783eb6f86483eee
luliannovi <luigimatteo.annovi@gmail.com>
2021-06-10 09:35:30 +0200

    10-06

--- 2021-05-21/2021-05-21/delete.c
+++ 2021-05-21/2021-05-21/delete.c
@@ -1,6 +1,10 @@
 #include "elemtype.h"
 #include "tree.h"
 
+Node* Get(Node* n) {
+	return n;
+}
+
 void VisitaDiscendenti(Node* n, const ElemType* key, Node* ok, Node* father) {
 	if (TreeIsLeaf(n))
 		return;
@@ -30,49 +34,119 @@ Node* SuccessivoEliminaPadre(Node* n) {
 	return tmp;
 }
 
-Node* Successivo(Node* n) {
+Node* Successivo(Node* n, Node ** padre) {
 	if (TreeIsEmpty(n))
 		return NULL;
 	Node* tmp = n->left;
+	*padre = Get(n);
 	while (tmp->left != NULL) {
+		*padre = Get(tmp);
 		tmp = tmp->left;
 	}
 	return tmp;
 }
 
-Node* DeleteBstNode(Node* n, const ElemType* key) {
-	if (TreeIsEmpty(n) || key==NULL)
-		return NULL;
-	Node* ok = NULL;
-	Node* total = n;
-	Node* father = NULL;
-	ElemType *k = key;
-	if (!TreeIsEmpty(n)) {
-		if (ElemCompare(&(n->value), k) == 0)
-			ok = n;
-		VisitaDiscendenti(n, k, ok,father);
-	}
-	if (TreeIsLeaf(ok)) {
-		father = NULL;
-		TreeDelete(ok);
-	}
-	else if (ok->left == NULL && ok->right != NULL) {
-		father = ok->right;
-		TreeDelete(ok);
-	}
-	else if (ok->left != NULL && ok->right == NULL) {
-		father = ok->left;
-		TreeDelete(ok);
-	}
-	else {
-		Successivo(ok)->left = ok->left;
-		Successivo(ok)->right = ok->right;
-		father = Successivo(ok);
-	
-		TreeDelete(SuccessivoEliminaPadre(ok));
-	}
-	
-	return total;
+Node* DeleteBstNodeRec(Node* n, const ElemType* key, Node * father) {
+	if (TreeIsEmpty(n))
+		return n;
+	if (ElemCompare(key, &n->value) == 0) {
+		if (TreeIsEmpty(father)) {
+			Node** p = malloc(sizeof(Node*));
+			Node* succ = Successivo(n,p);
+			if (succ == NULL) {
+				free(p);
+				return n->right;
+			}
+			if (TreeIsEmpty(TreeRight(succ))) {
+				succ->right = n->right;
+				succ->left = n->left;
+				p[0]->left = NULL;
+				free(p);
+				return succ;
+			}
+			else {
+				succ->right = n->right;
+				succ->left = n->left;
+				p[0]->left = succ->right;
+				free(p);
+				return succ;
+			}
+		}
+		else {
+			Node** p = malloc(sizeof(Node*));
+			Node* succ = Successivo(n, p);
+			if (succ == NULL) {
+				free(p);
+				return n->right;
+			}
+			if (TreeIsEmpty(TreeRight(succ))) {
+				succ->right = n->right;
+				succ->left = n->left;
+				p[0]->left = NULL;
+				free(p);
+				return succ;
+			}
+			else {
+				succ->right = n->right;
+				succ->left = n->left;
+				p[0]->left = succ->right;
+				free(p);
+				return succ;
+			}
+		}
+	}else if(ElemCompare(key,&n->value)<0)
+		n->left = DeleteBstNodeRec(n->left, key, n);
+	else
+		n->right = DeleteBstNodeRec(n->right, key, n);
+	return n;
 }
 
+bool Presente(Node* n, const ElemType* key) {
+	if (n == NULL)
+		return false;
+	if (ElemCompare(key, &n->value) == 0)
+		return true;
+	bool a = Presente(n->left, key);
+	if (a == true)
+		return true;
+	else {
+		a = Presente(n->right, key);
+		return a;
+	}
+}
 
+Node* DeleteBstNode(Node* n, const ElemType* key) {
+	if (TreeIsEmpty(n))
+		return n;
+	bool presente = Presente(n,key);
+
+
+	if (presente == false)
+		return n;
+	Node* tmp = DeleteBstNodeRec(n, key, NULL);
+	return tmp;
+}
+
+Node* TreeCreateFromVectorRec(const int* v, size_t v_size, int i) {
+	if (i >= (int)v_size) {
+		return NULL;
+	}
+
+	Node* l = TreeCreateFromVectorRec(v, v_size, i * 2 + 1);
+	Node* r = TreeCreateFromVectorRec(v, v_size, i * 2 + 2);
+
+	return TreeCreateRoot(&v[i], l, r);
+}
+
+Node* TreeCreateFromVector(const int* v, size_t v_size) {
+	return TreeCreateFromVectorRec(v, v_size, 0);
+}
+/*
+int main(void) {
+	int v[] = {5,4,7,2,3,6,8 };
+	size_t v_size = sizeof(v) / sizeof(int);
+	Node* tree = TreeCreateEmpty();
+	tree = TreeCreateFromVector(v, 7);
+	ElemType a = 7;
+	Node *b = DeleteBstNode(tree, &a);
+}*/
